9706651d8b68674ec489805806574584de7825c9
borutap <01151544@pw.edu.pl>
2022-03-24 13:35:54 +0100

    Class storing roles GUID

--- VaccinationSystem/VaccinationSystem/Data/Configurations/RoleSeedConfiguration.cs
+++ VaccinationSystem/VaccinationSystem/Data/Configurations/RoleSeedConfiguration.cs
@@ -11,19 +11,19 @@ namespace VaccinationSystem.Data.Configurations
             builder.HasData(
                 new IdentityRole
                 {
-                    Id = "c8076fe7-faf6-757b-3452-6aa5f7a33c6c", //random guid
+                    Id = RoleIdentifiers.Admin,
                     Name = "Administrator",
                     NormalizedName = "ADMINISTRATOR",
                 },
                 new IdentityRole
                 {
-                    Id = "53716615-3a3b-4948-9d28-8076bf328b4a", //random guid
+                    Id = RoleIdentifiers.Doctor, //random guid
                     Name = "Doctor",
                     NormalizedName = "DOCTOR",
                 },
                 new IdentityRole
                 {
-                    Id = "410adff7-f581-4737-b4d6-0dc9a88dec59", //random guid
+                    Id = RoleIdentifiers.Patient, //random guid
                     Name = "Patient",
                     NormalizedName = "PATIENT",
                 }
--- VaccinationSystem/VaccinationSystem/Data/Configurations/UserRoleSeedConfiguration.cs
+++ VaccinationSystem/VaccinationSystem/Data/Configurations/UserRoleSeedConfiguration.cs
@@ -11,17 +11,17 @@ namespace VaccinationSystem.Data.Configurations
             builder.HasData(
                 new IdentityUserRole<string>
                 {
-                    RoleId = "c8076fe7-faf6-757b-3452-6aa5f7a33c6c", //admin
+                    RoleId = RoleIdentifiers.Admin, //admin
                     UserId = "6f5f0ee8-640a-4645-ba8b-a4e3fa51b3dd"
                 },
                 new IdentityUserRole<string>
                 {
-                    RoleId = "410adff7-f581-4737-b4d6-0dc9a88dec59", //patient
+                    RoleId = RoleIdentifiers.Patient, //patient
                     UserId = "c1076fe7-abf6-420d-8810-6cb0f3a92f6a"
                 },
                 new IdentityUserRole<string>
                 {
-                    RoleId = "53716615-3a3b-4948-9d28-8076bf328b4a", //patient
+                    RoleId = RoleIdentifiers.Doctor, //doctor
                     UserId = "f1076fe7-abf6-420d-8810-6cb0f3a92f6a"
                 }
             );
