f1c736f41cd1d91acf22e38a22a02b3d9827e9d0
ZuzaTyszka <zuzia.tyszka@vp.pl>
2022-03-26 14:00:51 +0100

    vaccine seed configuration

--- VaccinationSystem/VaccinationSystem/Data/Configurations/VaccineSeedConfiguration.cs
+++ VaccinationSystem/VaccinationSystem/Data/Configurations/VaccineSeedConfiguration.cs
@@ -40,6 +40,54 @@ namespace VaccinationSystem.Data.Configurations
                     SerialNo = 223453464,
                     DiseaseId = -5,
                     RequiredDoses = 1
+                },
+                new Vaccine
+                {
+                    Id = -5, 
+                    Name ="Pfizer-21",
+                    SerialNo = 12354659,
+                    DiseaseId = -2,
+                    RequiredDoses = 2
+                },
+                new Vaccine
+                {
+                    Id = -6,
+                    Name = "Flu-21",
+                    SerialNo = 12315659,
+                    DiseaseId = -3,
+                    RequiredDoses = 1
+                },
+                new Vaccine
+                {
+                    Id = -7,
+                    Name = "Flu-22",
+                    SerialNo = 56591234,
+                    DiseaseId = -3,
+                    RequiredDoses = 1
+                },
+                new Vaccine
+                {
+                    Id = -8,
+                    Name = "Chickenpox",
+                    SerialNo = 42565323,
+                    DiseaseId = -4,
+                    RequiredDoses = 1
+                },
+                new Vaccine
+                {
+                    Id = -9,
+                    Name = "Measles",
+                    SerialNo = 1142565323,
+                    DiseaseId = -6,
+                    RequiredDoses = 1
+                },
+                new Vaccine
+                {
+                    Id = -10,
+                    Name = "Mumps",
+                    SerialNo = 13245125,
+                    DiseaseId = -7,
+                    RequiredDoses = 1
                 }
             );
         }
