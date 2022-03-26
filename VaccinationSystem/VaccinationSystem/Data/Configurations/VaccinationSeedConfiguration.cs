18d3efa038ea93fa3c0eb0d98a7137edbd44780b
ZuzaTyszka <zuzia.tyszka@vp.pl>
2022-03-26 14:31:18 +0100

    vaccination seed configuration

---
+++ VaccinationSystem/VaccinationSystem/Data/Configurations/VaccinationSeedConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace VaccinationSystem.Data.Configurations
+{
+    internal class VaccinationSeedConfiguration : IEntityTypeConfiguration<Vaccination>
+    {
+        public void Configure(EntityTypeBuilder<Vaccination> builder)
+        {
+            _ = builder.HasData(
+                new Vaccination
+                {
+                    Id = -1,
+                    //Patient = patient,        - nie umiem wstawić całych struktur, to się jakoś da czy może lepiej
+                    //                          w klasie Vaccination zamienić je na pojedyncze id?
+                    //Doctor = doctor,
+                    Status = VaccinationStatus.Planned,
+                    Date = new DateTime(2022, 4, 5),    // y, m, d
+                    //Vaccine = vaccine
+                },
+                new Vaccination
+                {
+                    Id = -2,
+                    //Patient = patient,
+                    //Doctor = doctor,
+                    Status = VaccinationStatus.Cancelled,
+                    Date = new DateTime(2022, 1, 15),
+                    //Vaccine = vaccine
+                },
+                new Vaccination
+                {
+                    Id = -1,
+                    //Patient = patient,
+                    //Doctor = doctor,
+                    Status = VaccinationStatus.Completed,
+                    Date = new DateTime(2022, 2, 12),
+                    //Vaccine = vaccine
+                }
+            );
+        }
+    }
+}
