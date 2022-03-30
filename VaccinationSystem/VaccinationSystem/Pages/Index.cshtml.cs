7f230fdc7751702168a3fe62d913f02454b74d0d
mateuszcichowicz <matcichowicz@gmail.com>
2022-03-30 04:24:45 +0200

    Poprawka

--- VaccinationSystem/VaccinationSystem/Pages/Index.cshtml.cs
+++ VaccinationSystem/VaccinationSystem/Pages/Index.cshtml.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using VaccinationSystem.Data;
-using VaccinationSystem.Data.Classes;
-using VaccinationSystem.IRepositories;
-using VaccinationSystem.Repositories;
 
 namespace VaccinationSystem.Pages
 {
@@ -11,13 +7,14 @@ namespace VaccinationSystem.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger, IAdministratorRepository p)
+        public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
         public void OnGet()
         {
+
         }
     }
 }
