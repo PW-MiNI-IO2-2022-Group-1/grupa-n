326cd2460814b4ac17d5410f515ddc17b510e7da
kowalakk <konradkowalak@gmail.com>
2022-05-24 01:24:11 +0200

    Revert "sprawdzenie czy reszta działa"
    This reverts commit c21bb0983cbdacea6e29520bcb7bcbb7b7c20e99.

--- VaccinationSystem/Tests/AutomatedUITests/LoginPageTests/RegisterPageTests.cs
+++ VaccinationSystem/Tests/AutomatedUITests/LoginPageTests/RegisterPageTests.cs
@@ -22,34 +22,34 @@ namespace Tests.AutomatedUITests.LoginPageTests
             Assert.Equal("Register - VaccinationSystem", _page.Title);
             Assert.Contains("Create a new account.", _page.Source);
         }
-        //[Fact]
-        //public void Register_WrongModelData_ReturnsErrorMessage()
-        //{
-        //    _page.ClickRegister();
-        //    Assert.Contains("The First name field is required.", _page.Source);
-        //    Assert.Contains("The Last name field is required.", _page.Source);
-        //    Assert.Contains("The Pesel field is required.", _page.Source);
-        //    Assert.Contains("The Email field is required.", _page.Source);
-        //    Assert.Contains("The Password field is required.", _page.Source);
-        //}
-        //[Fact]
-        //public void Register_WhenSuccessfullyExecuted_ReturnsConfirmMail()
-        //{
-        //    _page.PopulateFirstName("New Test");
-        //    _page.PopulateLastName("Patient");
-        //    _page.PopulatePesel("99112233445");
-        //    _page.PopulateEmail("newtest@patient.com");
-        //    _page.PopulatePassword("newtest1");
-        //    _page.PopulateConfirmPassword("newtest1");
-        //    _page.PopulateCity("city");
-        //    _page.PopulateZipCode("22-100");
-        //    _page.PopulateStreet("street");
-        //    _page.PopulateHouseNumber("5A");
-        //    _page.PopulateLocalNumber("7");
-        //    _page.ClickRegister();
-        //    Assert.Equal("Register - VaccinationSystem", _page.Title);
-        //    Assert.Contains("Register confirmation", _page.Source);
-        //}
+        [Fact]
+        public void Register_WrongModelData_ReturnsErrorMessage()
+        {
+            _page.ClickRegister();
+            Assert.Contains("The First name field is required.", _page.Source);
+            Assert.Contains("The Last name field is required.", _page.Source);
+            Assert.Contains("The Pesel field is required.", _page.Source);
+            Assert.Contains("The Email field is required.", _page.Source);
+            Assert.Contains("The Password field is required.", _page.Source);
+        }
+        [Fact]
+        public void Register_WhenSuccessfullyExecuted_ReturnsConfirmMail()
+        {
+            _page.PopulateFirstName("New Test");
+            _page.PopulateLastName("Patient");
+            _page.PopulatePesel("99112233445");
+            _page.PopulateEmail("newtest@patient.com");
+            _page.PopulatePassword("newtest1");
+            _page.PopulateConfirmPassword("newtest1");
+            _page.PopulateCity("city");
+            _page.PopulateZipCode("22-100");
+            _page.PopulateStreet("street");
+            _page.PopulateHouseNumber("5A");
+            _page.PopulateLocalNumber("7");
+            _page.ClickRegister();
+            Assert.Equal("Register - VaccinationSystem", _page.Title);
+            Assert.Contains("Register confirmation", _page.Source);
+        }
         public void Dispose()
         {
             _driver.Quit();
